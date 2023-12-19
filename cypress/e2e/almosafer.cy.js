048f5a202efe04019fede06f54e96885ab214155
shereen <shereennn10@gmail.com>
2023-12-19 03:32:05 +0300

    update

---
+++ cypress/e2e/almosafer.cy.js
@@ -0,0 +1,21 @@
+/// <reference types= "cypress" />
+//الشرح بفيديو 7
+const expectedCurrency = "SAR"
+
+// const expectedAttribute = "lang"
+//const expectedLanguage = "en"
+describe('Aspire test cases', () => {
+  it('check the currency is SAR', () => {
+    cy.visit('https://www.almosafer.com/en?ncr=1')
+    
+    cy.get('[data-testid="Header__CurrencySelector"]').invoke('text').should('include', expectedCurrency)
+
+    cy.wait(3000)
+  })
+  it('check the language of the website', () => {
+    cy.visit('https://www.almosafer.com/en?ncr=1')
+
+    cy.get('html').should('have.attr', 'lang' ,'en')
+    //cy.get('html').should('have.attr', 'expectedAttribute' ,'expectedLanguage')  او ممكن استخدم هاد الكود وبيعطي نفس النتيجة
+  });
+})
