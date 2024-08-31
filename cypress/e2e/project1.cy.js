eee88853bfd18898afad53df522d657a55d0807e
Shereen Abu Alrub <150395841+shereenabualrub@users.noreply.github.com>
2024-08-31 03:03:44 +0300

    Update and rename almosafer.cy.js to project1.cy.js

--- cypress/e2e/project1.cy.js
+++ cypress/e2e/project1.cy.js
@@ -1,9 +1,11 @@
 /// <reference types= "cypress" />
-// 7
-const expectedCurrency = "SAR"
 
-// const expectedAttribute = "lang"
-//const expectedLanguage = "en"
+
+
+const expectedCurrency = "SAR"
+const expectedAttribute = "lang"
+const expectedLanguage = "en"
+
 describe('test cases', () => {
   it('check the currency is SAR', () => {
     cy.visit('https://www.almosafer.com/en?ncr=1')
@@ -15,7 +17,7 @@ describe('test cases', () => {
   it('check the language of the website', () => {
     cy.visit('https://www.almosafer.com/en?ncr=1')
 
-    cy.get('html').should('have.attr', 'lang' ,'en')
-    //cy.get('html').should('have.attr', 'expectedAttribute' ,'expectedLanguage')  او ممكن استخدم هاد الكود وبيعطي نفس النتيجة
+    
+    cy.get('html').should('have.attr', 'expectedAttribute' ,'expectedLanguage')
   });
 })
